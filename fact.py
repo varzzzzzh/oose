e3909a9047613be840635478946d031a1e8003d8
Varsha R <163903349+varzzzzzh@users.noreply.github.com>
2025-05-02 01:27:15 +0530

    Update factorial without recursion

--- fact.py
+++ fact.py
@@ -1,1 +1,9 @@
-print("Hello")
+def factorial(n):
+    if n < 0:
+        return "Factorial not defined for negative numbers"
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+number = 7
+print(f"Factorial of {number} is {factorial(number)}")
